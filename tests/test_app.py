4e99030752ee83c099f63c4a1897c90cfe2d54cd
moonorb <serhan.turkmenler@gmail.com>
2023-06-04 18:43:49 -0400

    modified test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,19 @@
-import pytest
+import unittest
 from flask import Flask
+from flask.testing import FlaskClient
 
-@pytest.fixture
-def app():
-    app = Flask(__name__)
+from app.app import app
 
-    @app.route('/')
-    def index():
-        return 'Hello, World!'
 
-    return app
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
 
-def test_index(app):
-    client = app.test_client()
+    def test_index(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), 'Hits: 1')
 
-    response = client.get('/')
-    assert response.status_code == 200
+if __name__ == '__main__':
+    unittest.main()
 
